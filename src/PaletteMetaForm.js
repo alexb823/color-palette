0600c6e8dc5939796946ee9c22372d38d298d089
Alex Bukhman <alexb823@gmail.com>
2019-12-01 18:42:53 -0500

    finished palette form and emoji picker

--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { Picker } from 'emoji-mart';
+import 'emoji-mart/css/emoji-mart.css';
 
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -17,13 +19,20 @@ const PaletteMetaForm = props => {
     resetPaletteName,
   ] = useInputState('');
 
-  const { palettes, colors, history, savePalette, formOpen, hideForm } = props;
+  const [stage, setStage] = useState('form');
+  const { palettes, colors, history, savePalette, hideForm } = props;
 
-  const handleSubmit = () => {
+  const showEmojiPicker = event => {
+    event.preventDefault();
+    setStage('emoji');
+  };
+
+  const saveNewPalette = emoji => {
     const newPalette = {
       paletteName: newPaletteName,
       id: newPaletteName.toLocaleLowerCase().replace(/ /g, '-'),
       colors,
+      emoji: emoji.native,
     };
     savePalette(newPalette);
     resetPaletteName();
@@ -40,40 +49,55 @@ const PaletteMetaForm = props => {
   }, [newPaletteName]);
 
   return (
-    <Dialog
-      open={formOpen}
-      onClose={hideForm}
-      aria-labelledby="form-dialog-title"
-    >
-      <DialogTitle id="form-dialog-title">Choose a Palette Name</DialogTitle>
-      <ValidatorForm onSubmit={handleSubmit}>
-        <DialogContent>
-          <DialogContentText>
-            Please enter a name for your new palette. Make sure it's unique!
-          </DialogContentText>
-          <TextValidator
-            fullWidth
-            margin="normal"
-            label="Palette Name"
-            value={newPaletteName}
-            onChange={handlePaletteNameChange}
-            validators={['required', 'isPaletteNameUnique']}
-            errorMessages={[
-              'This field is required',
-              'Palette name must be unique',
-            ]}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={hideForm} color="primary">
-            Cancel
-          </Button>
-          <Button variant="contained" color="primary" type="submit">
-            Save Palette
-          </Button>
-        </DialogActions>
-      </ValidatorForm>
-    </Dialog>
+    <Fragment>
+      <Dialog
+        open={stage === 'form'}
+        onClose={hideForm}
+        aria-labelledby="choose-palette-name"
+      >
+        <DialogTitle id="choose-palette-name">
+          Choose a Palette Name
+        </DialogTitle>
+        <ValidatorForm onSubmit={showEmojiPicker}>
+          <DialogContent>
+            <DialogContentText>
+              Please enter a name for your new palette. Make sure it's unique!
+            </DialogContentText>
+            <TextValidator
+              fullWidth
+              margin="normal"
+              label="Palette Name"
+              value={newPaletteName}
+              onChange={handlePaletteNameChange}
+              validators={['required', 'isPaletteNameUnique']}
+              errorMessages={[
+                'This field is required',
+                'Palette name must be unique',
+              ]}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={hideForm} color="primary">
+              Cancel
+            </Button>
+            <Button variant="contained" color="primary" type="submit">
+              Save Palette
+            </Button>
+          </DialogActions>
+        </ValidatorForm>
+      </Dialog>
+
+      <Dialog
+        open={stage === 'emoji'}
+        onClose={hideForm}
+        aria-labelledby="choose-palette-emoji"
+      >
+        <DialogTitle id="choose-palette-emoji">
+          Choose a Palette Emoji
+        </DialogTitle>
+        <Picker title="Pick a Palette Emoji" onSelect={saveNewPalette} />
+      </Dialog>
+    </Fragment>
   );
 };
 

3c132208222d5b6289ba13cf194a030d8fd856de
Alex Bukhman <alexb823@gmail.com>
2020-01-05 18:23:05 -0500

    started on palette delete dialog

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -5,6 +5,19 @@ import { withStyles } from '@material-ui/core/styles';
 import MiniPalette from './MiniPalette';
 import styles from './styles/PaletteListStyles';
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+
 class PaletteList extends Component {
   constructor(props) {
     super(props);
@@ -37,6 +50,27 @@ class PaletteList extends Component {
             ))}
           </TransitionGroup>
         </div>
+        <Dialog open={true}>
+          <DialogTitle>Delete Palette?</DialogTitle>
+          <List>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <CheckIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Delete" />
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <CloseIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Cancel" />
+            </ListItem>
+          </List>
+        </Dialog>
       </div>
     );
   }
